7e969732a13d3bef6f3422fe414232f4e9d5026a
Ahmad-mufied <teranggelap01@gmail.com>
2023-02-13 17:58:13 +0700

    add explanation in these go files : query_params_test, response_code_test, server_test

--- query_param_test.go
+++ query_param_test.go
@@ -9,7 +9,22 @@ import (
 	"testing"
 )
 
+/*
+! Query Parameter
+*--	Query parameter adalah salah satu fitur yang biasa kita gunakan ketika membuat web
+*--	Query parameter biasanya digunakan untuk mengirim data dari client ke server
+*--	Query parameter ditempatkan pada URL
+*--	Untuk menambahkan query parameter, kita bisa menggunakan ?nama=value pada URL nya
+
+*/
+
 func sayHello(writer http.ResponseWriter, request *http.Request) {
+	/*
+	! url.URL
+	*--	Dalam parameter Request, terdapat attribute URL yang berisi data url.URL
+	*--	Dari data URL ini, kita bisa mengambil data query parameter yang dikirim dari client dengan menggunakan method Query() yang akan mengembalikan map
+
+	*/
 	name := request.URL.Query().Get("name")
 	if name == "" {
 		fmt.Fprint(writer, "Hello")
@@ -28,8 +43,14 @@ func TestQueryParamater(t *testing.T) {
 	body, _ := io.ReadAll(response.Body)
 
 	fmt.Println(string(body))
-
 }
+
+/*
+! Multiple Query Parameter
+*--	Dalam spesifikasi URL, kita bisa menambahkan lebih dari satu query parameter
+*--	Ini cocok sekali jika kita memang ingin mengirim banyak data ke server, cukup tambahkan query parameter lainnya
+*--	Untuk menambahkan query parameter, kita bisa gunakan tanda & lalu diikuti dengan query parameter berikutnya
+*/
 
 func MultipleQueryParameter(w http.ResponseWriter, r *http.Request) {
 	firstName := r.URL.Query().Get("first_name")
@@ -49,6 +70,15 @@ func TestMultipleQueryParameter(t *testing.T) {
 
 	fmt.Println(string(body))
 }
+
+/*
+! Multiple Value Query Parameter
+*--	Sebenarnya URL melakukan parsing query parameter dan menyimpannya dalam map[string][]string
+*--	Artinya, dalam satu key query parameter, kita bisa memasukkan beberapa value
+*--	Caranya kita bisa menambahkan query parameter dengan nama yang sama, namun value berbeda, misal :
+*	name=Eko&name=Kurniawan
+
+*/
 
 func MultipleParameterValues(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
